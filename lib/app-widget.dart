0a3840672f4ddec23c579611a61649ededf3a663
Bruno Monteiro <brunomoncar007@gmail.com>
2021-05-21 10:38:51 -0300

    separando os Widgets

---
+++ lib/app-widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Meu Primeiro App",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  var count = 0; //variável que controla a contagem
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple,
+      appBar: AppBar(
+        backgroundColor: Colors.purpleAccent,
+        title: Text("Contador"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "Contagem: $count",
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.purpleAccent,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meu_primeiro_app/app-widget.dart';
 
 void main() {
   runApp(AppWidget());
-}
-
-class AppWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Meu Primeiro App",
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  var count = 0; //variável que controla a contagem
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.purple,
-      appBar: AppBar(
-        backgroundColor: Colors.purpleAccent,
-        title: Text("Contador"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          "Contagem: $count",
-          style: TextStyle(color: Colors.white, fontSize: 18),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            count++;
-          });
-        },
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-        backgroundColor: Colors.purpleAccent,
-      ),
-    );
-  }
 }
