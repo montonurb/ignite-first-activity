91a2e7a821ac4189225ab0046d3fe897ccdef001
montonurb <brunomoncar007@gmail.com>
2021-05-20 21:27:08 -0300

    importando projeto aula-01 ignite-flutter

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Meu Primeiro App",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  var count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple,
+      appBar: AppBar(
+        backgroundColor: Colors.purpleAccent,
+        title: Text("Contador"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "Contagem: $count",
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.purpleAccent,
+      ),
+    );
+  }
+}
